28db26d12e16df85e7156d55c8cdd30d9250e3b6
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-09-03 11:24:26 +0300

    Add original numba version for comparinos

--- comparison/scripts/mahti_run.sh
+++ comparison/scripts/mahti_run.sh
@@ -12,4 +12,5 @@ ml cuda/11.5.0
 
 srun python3 python/hop_lbm.py 4000 4000 data/input.json hop
 srun python3 python/numba_lbm.py 4000 4000 data/input.json numba
+srun python3 python/02_Numba_indexing.py 4000 4000 data/input.json numba_original
 #srun python3 python/numpy_lbm.py 4000 4000 data/input.json numpy
